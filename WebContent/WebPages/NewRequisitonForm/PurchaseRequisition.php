7015d560208ba01dc37fe83c310bfeacb87251db
Aakriti Dubey <aakritidubey91@gmail.com>
2016-04-26 11:52:15 -0400

    Page 1 validations

--- WebContent/WebPages/NewRequisitonForm/PurchaseRequisition.php
+++ WebContent/WebPages/NewRequisitonForm/PurchaseRequisition.php
@@ -7,6 +7,12 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <style>
+  .reqd{
+	  color:red;
+	  font-size:125%;
+  }
+  </style>
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
   <script type="text/javascript"> $(function(){
@@ -17,6 +23,71 @@ $("#datepicker").datepicker({
 $("#datepicker").datepicker();
 
 });
+
+function validate(){
+	var err=0;
+	var form = document.forms["pDetails"];
+	var jc= document.forms["pDetails"]["JobCode"].value;
+	if(jc==""){
+		err=1;
+		$("#jobCodeDiv").addClass("has-error");
+	}
+	jc=document.forms["pDetails"]["suggvendor"].value;
+	if(jc==""){
+		err=1;
+		$("#svendordiv").addClass("has-error");
+	}
+	jc=document.forms["pDetails"]["shipAddr"].value;
+	if(jc==""){
+		err=1;
+		$("#saddrdiv").addClass("has-error");
+	}
+	jc=document.forms["pDetails"]["attn"].value;
+	if(jc==""){
+		err=1;
+		$("#attndiv").addClass("has-error");
+	}
+	jc=document.forms["pDetails"]["vendAddr"].value;
+	if(jc==""){
+		err=1;
+		$("#vaddrdiv").addClass("has-error");
+	}
+	jc=document.forms["pDetails"]["daten"].value;
+	if(jc==""){
+		err=1;
+		$("#datediv").addClass("has-error");
+	}
+	jc=document.forms["pDetails"]["email"].value;
+	if(jc==""){
+		err=1;
+		$("#emaildiv").addClass("has-error");
+	}
+	jc=document.forms["pDetails"]["shipMethod"].value;
+	if(jc==""){
+		err=1;
+		$("#smdiv").addClass("has-error");
+	}
+	jc=document.forms["pDetails"]["shipMethod"].value;
+	if(jc==""){
+		err=1;
+		$("#smdiv").addClass("has-error");
+	}
+	jc=document.forms["pDetails"]["bcs"].value;
+	if(document.getElementById("budgt").checked && jc==""){
+		err=1;
+		$("#budgdiv").addClass("has-error");
+	}
+	jc=document.forms["pDetails"]["explain"].value;
+	if(document.getElementById("nbudgt").checked && jc==""){
+		err=1;
+		$("#nbudgtdiv").addClass("has-error");
+	}
+	if(err==1){
+		alert("Fill out all the required fields");
+		return false;
+	}
+	return true;
+}
 
     
 </script>
@@ -46,13 +117,13 @@ $("#datepicker").datepicker();
  		 </div>
 	</div>
       
-      <form action="Items.php" method="post" role="form">
+      <form name="pDetails" action="Items.php" method="post" role="form">
       <div class="container">
       <div class="container" id="initial" style="padding: 10px;">
       <div class="col-sm-4"><label for="requester">Requested By:</label><label id="requesterName">Aakriti Dubey</label></div>
-      <div class="col-sm-6 form-inline form-group pull-right">
+      <div  class="col-sm-6 form-inline form-group pull-right">
         <label class="col-sm-3 control-label" for="ddown">Job Code:</label>
-        <div class="form-inline selectContainer">
+        <div id="jobCodeDiv" class="form-inline selectContainer">
             <select class="form-control" name="JobCode" id="ddown">
                 <option value="">Job Code</option>
                 <option value="US13003 - Tigershark Rework">US13003 - Tigershark Rework</option>
@@ -67,9 +138,8 @@ $("#datepicker").datepicker();
 			     <table class="table table-bordered" style="padding:10px">
 			     <tbody>
 			      <tr>
-			        <td colspan="2" class="col-sm-4"><label for="svendor">Suggested Vendor:</label> 
-			        <!--<input type="text" class="form-control" id="svendor" name="suggvendor"/></td>-->
-					<div class="form-inline selectContainer">
+			        <td colspan="2" class="col-sm-4"><label for="svendor">Suggested Vendor:<span class="reqd">*</span></label> 
+					<div id="svendordiv" class="form-inline selectContainer">
 						<select class="form-control" name="suggvendor" id="svendor">
 							<option value="">Suggested Vendor</option>
 							<option value="US13003">US13003 - Tigershark Rework</option>
@@ -79,33 +149,33 @@ $("#datepicker").datepicker();
 						</select>
 					</div>
 					</td>
-			        <td class="col-sm-4"><label for="shipAddress">Shipping Address:</label> 
+			        <td id="saddrdiv" class="col-sm-4"><label for="shipAddress">Shipping Address:<span class="reqd">*</span></label> 
 			        <textarea class="form-control" rows="4" id="shipAddress" name="shipAddr"></textarea> </td>
-			        <td class="col-sm-4"> <div class="checkbox"><label class="checkbox-inline"><input type="checkbox" value="" name="bugd">Budgeted</label>
+			        <td class="col-sm-4"> <div class="checkbox"><label class="checkbox-inline"><input type="checkbox" value="" name="bugd" id="budgt" checked>Budgeted</label>
 						</div> 
-						<label for="bcs">BCS#:</label><input type="text" class="form-control" id="bcs" name="bcs"/></td>
+						<div id="budgdiv"><label for="bcs">BCS#:</label><input  type="text" class="form-control" id="bcs" name="bcs"/></div></td>
 			      </tr>
 			      <tr>
-			        <td colspan="2" class="col-sm-4"><label for="vendorAddress">Address of Vendor:</label> 
+			        <td id="vaddrdiv" colspan="2" class="col-sm-4"><label for="vendorAddress">Address of Vendor:<span class="reqd">*</span></label> 
 			        <textarea class="form-control" rows="4" id="vendorAddress" name="vendAddr"></textarea> </td>
-			        <td class="col-sm-4"><label for="attn">Attention:</label> 
+			        <td id="attndiv" class="col-sm-4"><label for="attn">Attention:<span class="reqd">*</span></label> 
 			        <input type="text" class="form-control" id="attn" name="attn"/></td>
-			        <td class="col-sm-4"> <div class="checkbox"><label class="checkbox-inline"><input type="checkbox" value="" name="nbug">Non-Budgeted</label>
+			        <td  class="col-sm-4"> <div class="checkbox"><label class="checkbox-inline"><input type="checkbox" value="" name="nbug" id="nbudgt">Non-Budgeted</label>
 						</div> 
-						<label for="explain">Explanation:</label><input type="text" class="form-control" id="explain" name="explain"/></td>
+						<div id="nbudgtdiv"><label for="explain">Explanation:</label><input type="text" class="form-control" id="explain" name="explain"/></div></td>
 			      </tr>
 			      <tr>
 			         <td class="col-sm-2"><label for="phoneNum">Phone Number:</label> 
 			        <input type="text" class="form-control" id="phoneNum" name="phoneNum"/></td>
 			        <td class="col-sm-2"><label for="faxNum">Fax Number:</label> 
 			        <input type="text" class="form-control" id="faxNum" name="faxNum"/></td>
-			        <td class="col-sm-4 form-control" >
-                <label for="datepicker">Date Needed:</label>
+			        <td id="datediv" class="col-sm-4 form-control" >
+                <label for="datepicker">Date Needed:<span class="reqd">*</span></label>
                     <div class="input-group date" data-provide="datepicker">
     <input type="text" class="form-control" id="datepicker" name="daten"></div></td>
 			         <td rowspan ="2" class="col-sm-4"> 
 					  <div class="row-sm-2 radio">
-			         <label><input type="radio" name="scope" value="pind" >PARI India Scope</label>
+			         <label><input type="radio" name="scope" value="pind" checked>PARI India Scope</label>
 						</div> 
 						<div class="row-sm-2 radio"><label><input type="radio" name="scope" value="pinc">PARI Inc Scope</label>
 						</div>
@@ -114,9 +184,9 @@ $("#datepicker").datepicker();
 					 </td>
 				</tr>
 			      <tr>
-			        <td colspan="2" class="col-sm-4"><label for="email">Email:</label> 
+			        <td id="emaildiv" colspan="2" class="col-sm-4"><label for="email">Email:<span class="reqd">*</span></label> 
 			        <input type="email" class="form-control" id="email" name="email"/></td>
-			        <td class="col-sm-4"><label for="shipMethod" >Shipping Method:</label> 
+			        <td id="smdiv" class="col-sm-4"><label for="shipMethod" >Shipping Method:<span class="reqd">*</span></label> 
 			       <input type="text" class="form-control" id="shipMethod" name="shipMethod"/></td>
 			        
 			      </tr>
@@ -124,7 +194,7 @@ $("#datepicker").datepicker();
 			  </table>
 			  </div>
 			  <div>
-			  <button class="btn btn-default pull-right" type="Submit">Next</button>
+			  <button class="btn btn-default pull-right" type="Submit" onclick="return validate();">Next</button>
 			  </div>
 			  </form>
 			</div>
