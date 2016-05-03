b61b6f54e1c244ec13804d50d434c1d4eeeaf7bb
Aakriti Dubey <aakritidubey91@gmail.com>
2016-05-03 15:31:55 -0400

    Adding the budget allocation page; minor changes

--- WebContent/WebPages/NewRequisitonForm/PurchaseRequisition.php
+++ WebContent/WebPages/NewRequisitonForm/PurchaseRequisition.php
@@ -124,8 +124,7 @@ if (!$conn) {
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-     <!-- <a clss="navbar-brand" href="index.html"><img src="../Images/parilogo.PNG" alt="PARI"/></a> -->
-     <a class="navbar-brand" href="#">PARI Purchase Requisition</a>
+    <a class="navbar-brand" href="#">PARI Purchase Requisition</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">New Requisition</a></li>
