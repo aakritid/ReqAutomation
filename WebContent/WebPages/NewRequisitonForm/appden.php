6f1ddde6283047ad6bc0c6a117b7ff4bae34c791
Aakriti Dubey <aakritidubey91@gmail.com>
2016-06-06 11:03:10 -0400

    Add confirmation modal for Approve.

--- WebContent/WebPages/NewRequisitonForm/appden.php
+++ WebContent/WebPages/NewRequisitonForm/appden.php
@@ -14,12 +14,12 @@ if($_POST['request']=='process'){
 	$reqid= $_SESSION['SReq'];
 	$request=$_POST['reqtype'];
 	
-	if($request=='Approve'){
+	if($request=='approveConfirm'){
 		$qry="insert into approval (ReqId, AppDen) values (".$reqid.",0)";
 		echo (0);
 	}
 	
-	else if($request=='Confirm'){
+	else if($request=='denyConfirm'){
 		$reason=$_POST['reason'];
 		$qry="insert into approval (ReqId, AppDen, Reason) values (".$reqid.",1,'".$reason."')";
 		echo (1);
